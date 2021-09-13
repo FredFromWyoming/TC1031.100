96fb5258fec5d08d181cda6996b611f778c75460
FredFromWyoming <60443766+FredFromWyoming@users.noreply.github.com>
2021-09-12 22:23:40 -0500

    Add files via upload

---
+++ Bike.cpp
@@ -0,0 +1,13 @@
+#include "Bike.h"
+#include <string>
+using namespace std;
+
+void Bike::setDisplacement(int x){
+    engineDisplacement = x;
+}
+
+int Bike::getDisplacement(){
+    return engineDisplacement;
+}
+
+
